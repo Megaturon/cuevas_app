03392d57f6483acba31bf011db32bc7281c0daaf
Renzo Cuevas <cuevas_renzo@plpasig.edu.ph>
2026-02-14 17:30:54 +0800

    Exercise #2 - Laravel Architectural Concepts

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Services\UserService;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function show() {
+        $users = [
+            [
+                'name' => 'Bob Cruz',
+                'gender' => 'Male'
+            ],
+            [
+                'name' => 'Alice Gomez',
+                'gender' => 'Female'
+            ]
+        ];
+        return response()->json($users);
+    }
+
+    public function index(UserService $userService) {
+        return $userService->listUsers();
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,31 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use App\Http\Controllers\UserController;
+use App\Services\UserService;
+use Illuminate\Support\Facades\Response;
 
 Route::get('/', function () {
-    return view('home');
+    return view('welcome');
+});
+
+//Controller
+Route::get('/show-users', [UserController::class, 'show']);
+
+//Service Container
+Route::get('/test-container', function(Request $request) {
+    $input = $request->input('key');
+    return $input;
+});
+
+//Service Providers 
+Route::get('/test-provider', function(UserService $userService) {
+    return $userService->listUsers();
+});
+
+Route::get('/test-users', [UserController::class, 'index']);
+
+Route::get('/test-facade', function(UserService $userService){
+    return Response::json($userService->listUsers());
 });
